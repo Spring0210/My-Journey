7e7df092c7e4c9404c793e9e3c9d78ee71c7db51
Ben Xu <spring@mac.home>
2025-05-08 05:44:07 -0400

    Create project structure

---
+++ src/main/java/com/myjourney/MyJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.myjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJourneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJourneyApplication.class, args);
+    }
+
+}
